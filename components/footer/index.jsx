e5006a5f27b5bb231cf731408b50f9fa5c909ad8
zappjs-test <31462589+zappjs-test@users.noreply.github.com>
2017-09-02 00:59:35 -0500

    generates files

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class  extends Component {
+    render() {
+        return (
+            <div className={styles.footer}>
+    Copyright
+</div>
+        );
+    }
 }
 
 export default connect()(Home);
--- components/header/index.jsx
+++ components/header/index.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Header extends Component {
+    render() {
+        return (
+            <div className={styles.header}>
+    <h1>
+        {this.props.title}
+    </h1>
+</div>
+        );
+    }
 }
 
 Header.defaultProps({
