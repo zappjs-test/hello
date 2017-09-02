65b78415f033147f8a01bdfb808da8b86acd3a14
zappjs-test <31462589+zappjs-test@users.noreply.github.com>
2017-09-02 00:58:16 -0500

    generates files

---
+++ components/footer/index.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class  extends Component {
+}
+
+export default connect()(Home);
---
+++ components/header/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class  extends Component {
+}
+
+.defaultProps({
+    title: 'Hello'
+});
+
+.propTypes = {
+    title: PropTypes.string
+};
+
+export default connect()(Home);
